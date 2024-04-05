7ad4f8766845b6c933a68e95baec4b096f8aaea2
Ming-Jung YU <minayu416@gmail.com>
2024-04-06 00:19:16 +0800

    deploy new ver.

--- _next/static/chunks/app/[lng]/about/layout-a24e1a9aef51f07d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[421,419,907,583,867,478,303,264],{3521:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
