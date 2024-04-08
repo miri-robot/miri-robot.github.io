d043badda028df1e1f175c81f46a69221c79864e
Ming-Jung YU <minayu416@gmail.com>
2024-04-08 23:00:10 +0800

    fixed incorrect version info and fixed poe button hided in introduction

--- _next/static/chunks/app/[lng]/[method]/introduction/layout-388c4552e1060f0d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[303,419,421,907,478,583,264,867],{3521:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
